8011ed36865842fd44a63ec0df8c71be4cf78968
Fancy2209 <paulo.medeiros1302@gmail.com>
2026-05-29 17:31:33 +0000

    Disable Verbose tar

--- scripts/007-ps3libraries.sh
+++ scripts/007-ps3libraries.sh
@@ -5,7 +5,7 @@
 wget --no-check-certificate https://github.com/ps3dev/ps3libraries/tarball/master -O ps3libraries.tar.gz
 
 ## Unpack the source code.
-rm -Rf ps3libraries && mkdir ps3libraries && tar --strip-components=1 --directory=ps3libraries -xvzf ps3libraries.tar.gz && cd ps3libraries
+rm -Rf ps3libraries && mkdir ps3libraries && tar --strip-components=1 --directory=ps3libraries -xzf ps3libraries.tar.gz && cd ps3libraries
 
 ## Compile and install.
 ./libraries.sh
